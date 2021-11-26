828a7ef8b0be24e7e214a23bb48efe195d168d25
dipacinakennedy <95093302+dipacinakennedy@users.noreply.github.com>
2021-11-27 00:28:06 +0800

    Add files via upload

---
+++ programming activty1.php
@@ -0,0 +1,55 @@
+
+<html>
+<head>
+	<title>Programming activty #1 </title>
+</head>
+<body>
+
+	
+<div 
+
+	<h1> Programming activty #1 </h1>
+
+	<p> Enter your Name: <input type="text" ng-model="Name"></p>
+	<p> Your Name is: <spam ng-bind="Name"></spam></p>
+
+	<p> Enter your address: <input type="text" ng-model="address"></p>
+	<p> Your address is: <span ng-bind="address"></span></p>
+
+	<p>Enter your Senior High School : <input type="text" ng-model="shs></p>
+	<p> Your Senior high School is: <spam ng-bind="shs"></spam></p>
+
+	<p> Enter your Course: <input type="text" ng-model="Course"></p>
+	<p> Your  Senior High School is: <spam ng-bind="shs"></spam></p>
+
+	<p>Enter your course :<input type="text"ng-model= "course"></p>
+	<p>Your Course is: <span ng-bind="course"></p>
+
+	<h1>PROGRAMMING ACTIVITY #2</h1>
+	
+
+
+</div>
+		<p><b>List of Fruits</b></p>
+	<ol>
+	<li ng-repeat=" mp in list of food">
+    <li>fruit grapes color violet description small</li>
+	<li>fruit banana color yellow  description small</li>
+	<li>fruit watermellon color green description big </li>
+	<li>fruit ponkan color orange   description big</li>
+	<li>fruit melon color peach description big</li>
+	<li>fruit chico color brown description small</li>
+	<li>fruit strawbey color pink  description small</li>
+	<li>fruit indian mango color green  description small</li>
+	<li>fruit cherry color reed  description small</li>
+	</ol>
+</div>
+
+
+</div>
+<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1,3,14/angular.min.js"></script>
+
+
+
+</body>
+</html>
